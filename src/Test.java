330a5a76495a85d1570a8f0c58f77590430d8e8f
YannickBougaran <yannick.nincrafter@gmail.com>
2022-09-27 09:12:38 +0200

    27092022

--- src/Test.java
+++ src/Test.java
@@ -5,20 +5,10 @@ public class Test {
     public static void main(String args[]){
 
         Vertex v1 = new Vertex(4, 3);
-        Vertex v2 = new Vertex(34, 2);
-        Vertex vv = new Vertex(0, 0);
+        Vertex v2 = new Vertex(5, 10);
+        Vertex v3 = new Vertex(0, 0);
+        Vertex v4 = new Vertex(4, 3);
 
-        /*System.out.println(v1);
-        System.out.println(v1.distance());
-
-        System.out.println(v2);
-        System.out.println(v2.distance());
-
-        System.out.println(vv);
-        System.out.println(vv.distance());
-        */
-
-        v1.skalarMultMod(3);
-        System.out.println(v1);
+        System.out.println(v1.distance(v2));
     }
 }
--- src/Vertex.java
+++ src/Vertex.java
@@ -10,9 +10,40 @@ public class Vertex {
 
     public String toString(){return "("+x+","+y+")";}
 
-    public Double distance(){return Math.sqrt(x*x + y*y);}
+    public double distance(Vertex that){
+        return(Math.sqrt((that.x - this.x * that.x - this.x)  + (that.y - this.y * that.y - this.y)));
+    }
+    public double lenght(){return Math.sqrt(x*x + y*y);}
 
-    public Vertex skalarMult(double s){return new Vertex(x*s, y*s);}
+    public Vertex skalarMult(double s){return new Vertex(x*s,y*s);}
 
-    public void skalarMultMod(double s){this.x=s*x; this.y=s*y;}
+    public void skalarMultMod(double s) {
+        x = s * x;
+        y = s * y;
+    }
+
+    public Vertex add(Vertex v2){return new Vertex(x+v2.x, y+v2.y);}
+    public Vertex sub(Vertex v2){return new Vertex(this.x-v2.x, this.y-v2.y);}
+
+    public void addMod(Vertex v2){this.x=x+v2.x;}
+
+    public double getX(){return x;}
+    public double getY(){return x;}
+
+    public void setX(double x){this.x=x;}
+    public void setY(double y){this.y=y;}
+
+    public boolean equals(Object thatObject){
+        if(thatObject instanceof Vertex){
+            Vertex that = (Vertex)thatObject;
+            return this.x==that.x && this.y==that.y;
+        }
+        return false;
+    }
+
+    public Vertex normalized(){
+        double factor = this.lenght();
+
+        return new Vertex(this.x=x/factor, this.y=y/factor);
+    }
 }
