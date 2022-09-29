623fa4af58755ba481b69d8ced276c08ae637327
YannickBougaran <yannick.nincrafter@gmail.com>
2022-09-29 15:25:31 +0200

    29092022

--- src/GeometricObject.java
+++ src/GeometricObject.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class GeometricObject {
 
     Vertex pos;
@@ -16,51 +18,53 @@ public class GeometricObject {
         if(pos.y<0){this.pos.y=-pos.y;}
     }
 
-    public GeometricObject(double x, double y, double width, double height){
-        this(new Vertex(x,y), width, height);
+    public GeometricObject(double x, double y, double width, double height){this(new Vertex(x,y), width, height);}
+
+    public GeometricObject(double width, double height){this(0,0, width, height);}
+
+    public GeometricObject(double width){this(width, width);}
+
+    public GeometricObject(){this(0, 0, 10, 10);}
+
+    public String toString() {return "pos=" + pos + ", width=" + width + ", height=" + height + '}';}
+
+    public double circumference(){return (width + height) * 2;}
+
+    public double area(){return width * height;}
+
+    public boolean contains(Vertex v1){return(v1.x >= this.pos.x && v1.x + width <= this.pos.x && v1.y >= this.pos.y && v1.y - height <= this.pos.y);}
+
+    public boolean isLargerThan(GeometricObject g1) {return this.area() > g1.area();}
+
+    public void moveTo(Vertex v1){this.pos = v1;}
+
+    public void moveTo(double x, double y){moveTo(new Vertex(x, y));}
+
+    public void move(Vertex v1){moveTo(this.pos.add(v1));}
+
+    public Vertex getPos() {return pos;}
+
+    public void setPos(Vertex pos) {this.pos = pos;}
+
+    public double getWidth() {return width;}
+
+    public void setWidth(double width) {this.width = width;}
+
+    public double getHeight() {return height;}
+
+    public void setHeight(double height) {this.height = height;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeometricObject that = (GeometricObject) o;
+        return Double.compare(that.width, width) == 0 && Double.compare(that.height, height) == 0 && pos.equals(that.pos);
     }
 
-    public GeometricObject(double width, double height){
-        this(0,0, width, height);
+    public boolean contains(GeometricObject that){
+        return that.pos.x >= this.pos.x && that.pos.x <= this.pos.x+width &&
+                that.pos.y >= this.pos.y && that.pos.y <= this.pos.y+height ||
+                ;
     }
-
-    public GeometricObject(double width){
-        this(width, width);
-    }
-
-    public GeometricObject(){
-        this(0, 0, 10, 10);
-    }
-
-    public String toString() {
-        return "pos=" + pos +
-                ", width=" + width +
-                ", height=" + height +
-                '}';
-    }
-
-    public Vertex getPos() {
-        return pos;
-    }
-
-    public void setPos(Vertex pos) {
-        this.pos = pos;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
 }
